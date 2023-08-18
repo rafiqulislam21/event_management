f1df5bd74802ef481a36a74a0b0ba72591dce81c
Md Rafiqul Islam <ribijoy19@gmail.com>
2023-08-18 20:17:48 +0200

    dark mode added

--- em_frontend/src/components/NavBar.vue
+++ em_frontend/src/components/NavBar.vue
@@ -1,10 +1,3 @@
-<!-- <template>
-  <div id="nav">
-    <router-link to="/"><v-btn>Home</v-btn></router-link> |
-    <router-link to="/about"><v-btn>About</v-btn></router-link> |
-    <router-link to="/bookmarks"><v-btn>Bookmarks</v-btn></router-link>
-  </div> -->
-
 <template>
   <v-layout>
     <v-app-bar color="light" density="compact">
@@ -23,10 +16,67 @@
             <v-badge :content="$store.state.bookmarked" color="error">
               <v-icon>mdi-bell-outline</v-icon>
             </v-badge>
-          </v-btn>  
+          </v-btn>
         </router-link>
+        <v-btn @click="toggleTheme" :icon="isDarkMode ? 'mdi-white-balance-sunny' : 'mdi-weather-night'"></v-btn>
         <v-btn icon="mdi-dots-vertical"></v-btn>
       </template>
     </v-app-bar>
   </v-layout>
 </template>
+
+
+<script>
+import {useTheme } from 'vuetify'
+
+
+export default {
+  name: "NavBar",
+  setup() {
+    const theme = useTheme()    
+    return {
+      theme,
+      toggleTheme: () => {
+        // this.isDarkMode = !this.isDarkMode;
+        // console.log(isDarkMode)
+        theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+      }
+    }
+  },
+  data: () => ({
+    isDarkMode: false
+  }),
+
+  // mounted() {
+  //   const theme = localStorage.getItem("dark_theme");
+  //   if (theme) {
+  //     if (theme === "true") {
+  //       this.$vuetify.theme.dark = true;
+  //     } else {
+  //       this.$vuetify.theme.dark = false;
+  //     }
+  //   } else if (
+  //     window.matchMedia &&
+  //     window.matchMedia("(prefers-color-scheme: dark)").matches
+  //   ) {
+  //     this.$vuetify.theme.dark = true;
+  //     localStorage.setItem(
+  //       "dark_theme",
+  //       this.$vuetify.theme.dark.toString()
+  //     );
+  //   }
+  // },
+  methods: {
+    // handleDarkMode() {
+    //   // this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+    //   this.isDarkMode = !this.isDarkMode;
+
+    //   const theme = useTheme()
+    //   theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+
+    //   // this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+    //   // localStorage.setItem("dark_theme", this.$vuetify.theme.dark.toString());
+    // }
+  }
+}
+</script>
--- em_frontend/src/plugins/vuetify.js
+++ em_frontend/src/plugins/vuetify.js
@@ -1,9 +1,23 @@
 // Styles
 import "@mdi/font/css/materialdesignicons.css";
 import "vuetify/styles";
-
+import colors from 'vuetify/lib/util/colors'
 // Vuetify
 import { createVuetify } from "vuetify";
 
-export default createVuetify();
+export default new createVuetify({
+  theme: {
+    // defaultTheme: 'dark',
+    themes: {
+      light: {
+        primary: colors.green.accent3,
+        secondary: colors.deepOrange.lighten1
+      },
+      dark: {
+        primary: '#90CAF9',
+        secondary: '#E91E63'
+      },
+    },
+  },
+});
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
